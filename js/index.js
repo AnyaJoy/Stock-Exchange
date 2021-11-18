c4390ae87daeaf4a51a403ddaafbcb5de9609c1d
AnyaJoy <88624249+AnyaJoy@users.noreply.github.com>
2021-11-18 15:18:56 +0200

    Fixed search

--- js/index.js
+++ js/index.js
@@ -2,163 +2,169 @@ const searchInput = document.getElementById("searchInput");
 
 const searchButton = document.getElementById("searchButton");
 
-const search_result_space = document.getElementsByClassName("search_result_space");
+const search_result_space = document.getElementById("suggestions");
 
 const body = document.getElementById("body");
 
-const tringle_wrapper = document.getElementById("tringle_wrapper")
+const tringle_wrapper = document.getElementById("tringle_wrapper");
 
-
-
-
+const searchWrapper = document.getElementById("search_wrapper");
 
 // loader
-const loader = document.querySelector("#loading");
+const loader = document.querySelector(".loader");
 
-// showing loading
-function displayLoading() {
-    loader.classList.add("display");
-    // to stop loading after some time
-    // setTimeout(() => {
-    //     loader.classList.remove("display");
-    // }, 4000);
-}
+searchInput.addEventListener("input", function () {
+    if ((e.which == 13) | (searchInput.value == "")){
+        return false;
+      }
 
-// hiding loading 
-function hideLoading() {
-    loader.classList.remove("display");
-}
+    if (searchInput.value !== "") {
+    searchButton.addEventListener("click", searchCompanies);
+    searchButton.classList.add("able");
+    searchCompanies();
+    tringle_wrapper.style.display = "block";
+  } else {
+    console.log("input is empty");
+    search_result_space.style.display = "none";
+    tringle_wrapper.style.display = "none";
+    searchButton.removeEventListener("click");
+    searchButton.classList.remove("able");
+  }
+});
 
-searchInput.addEventListener("input", displayLoading);
-searchInput.addEventListener("input", displaySearchResultSpace);
-
-
-// нажатие кнопки на enter!!!
-document.getElementById('search_wrapper').addEventListener('submit', function(e) {
+// searching on enter
+searchWrapper.addEventListener(
+  "submit",
+  function (e) {
     searchCompanies();
     e.preventDefault();
-}, false);
-
-const debounce = (func, delay) => {
-    let debounceTimer
-    return function() {
-        const context = this
-        const args = arguments
-            clearTimeout(debounceTimer)
-                debounceTimer
-            = setTimeout(() => func.apply(context, args), delay)
-    }
-} 
-
-searchInput.addEventListener("keyup", debounce(function() {
-    searchCompanies()
-        }, 2000));
-
-function displaySearchResultSpace() {
-    tringle_wrapper.style.display = "block";
-};
+  },
+  false
+);
 
 const searchCompanies = () => {
-    displayLoading();
-    const userInput = searchInput.value;
-    const stocksUrl = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query="+userInput+"&limit=10&exchange=NASDAQ";
-    fetch(stocksUrl)
-      .then((response) => response.json())
-      .then((data) => {
-            suggestions.innerHTML = ''; 
-        
-            for (let i = 0; i < data.length; i++) {
-                        
-            const searchedCompany = document.createElement("a");
+  searchButton.style.display = "none";
+  loader.style.display = "block";
+  search_result_space.style.display = "flex";
+  const userInput = searchInput.value;
 
-            searchedCompany.setAttribute("id","suggestions_element");
+  const stocksUrl =
+    "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=" +
+    userInput +
+    "&limit=10&exchange=NASDAQ";
+  fetch(stocksUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      suggestions.innerHTML = "";
 
-            searchedCompany.href = './html/company.html?symbol=' + data[i]["symbol"];
+      for (let i = 0; i < data.length; i++) {
+        const searchedCompany = document.createElement("a");
 
-            const url = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/" + data[i]["symbol"];
-                fetch (url)
-                .then ((response2) => response2.json())
-                .then((data2) => {
-                    let percentages = data2.profile["changesPercentage"];
-                    
-                    if (percentages === "(0%)" | percentages === "0.0") {} else
-                    {percentages = parseFloat(percentages).toFixed(2)};
+        searchedCompany.setAttribute("id", "suggestions_element");
 
-                    let stockPercentages = document.createElement("span");
-                    stockPercentages.setAttribute("id","stockPercentages");
-                    stockPercentages.append(percentages);
-                    stockPercentages.setAttribute("onerror","this.style.display='none'");
+        searchedCompany.href =
+          "./html/company.html?symbol=" + data[i]["symbol"];
 
-                    if (percentages < 0) {
-                        stockPercentages.innerHTML = `<span style="color: red" >(` + percentages + `%)</span>`;  
-                    } else {
-                        stockPercentages.innerHTML = `<span style="color: #33D759FF" >(+` + percentages + `%)</span>`; 
-                    };
+        const url =
+          "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/" +
+          data[i]["symbol"];
+        fetch(url)
+          .then((response2) => response2.json())
+          .then((data2) => {
+            let percentages = data2.profile["changesPercentage"];
 
-                    if (percentages === "0.0") {
-                        stockPercentages.innerHTML = `<span style="color: black" >(` + percentages + `%)</span>`;  
-                    };  
+            if ((percentages === "(0%)") | (percentages === "0.0")) {
+            } else {
+              percentages = parseFloat(percentages).toFixed(2);
+            }
 
-                    if (percentages === "(0%)") {
-                        stockPercentages.innerHTML = `<span style="color: black" >` + percentages + `</span>`; 
-                    };
+            let stockPercentages = document.createElement("span");
+            stockPercentages.setAttribute("id", "stockPercentages");
+            stockPercentages.append(percentages);
+            stockPercentages.setAttribute(
+              "onerror",
+              "this.style.display='none'"
+            );
 
-                    let companyNameSearched = document.createElement("span");
-                    companyNameSearched.setAttribute("id","companyName");
-                    companyNameSearched.innerHTML = data[i]["name"];
+            if (percentages < 0) {
+              stockPercentages.innerHTML =
+                `<span style="color: red" >(` + percentages + `%)</span>`;
+            } else {
+              stockPercentages.innerHTML =
+                `<span style="color: #33D759FF" >(+` +
+                percentages +
+                `%)</span>`;
+            }
 
-                    let companySymbolSearched = document.createElement("span");
-                    companySymbolSearched.setAttribute("id","companySymbol");
-                    companySymbolSearched.innerHTML = "  (" + data[i]["symbol"] + ")  ";
+            if (percentages === "0.0") {
+              stockPercentages.innerHTML =
+                `<span style="color: black" >(` + percentages + `%)</span>`;
+            }
 
-                    if (userInput !== "") {
-                        let text =  companyNameSearched.innerHTML;
-                        let re = new RegExp(userInput,"gi");
-                        let newText = text.replace(re, `<span id="mark">$&</span>`);
-                        companyNameSearched.innerHTML = newText;
-                    }
+            if (percentages === "(0%)") {
+              stockPercentages.innerHTML =
+                `<span style="color: black" >` + percentages + `</span>`;
+            }
 
-                    if (userInput !== "") {
-                        let text =  companySymbolSearched.innerHTML;
-                        let re = new RegExp(userInput,"gi");
-                        let newText = text.replace(re, `<span id="mark">$&</span>`);
-                        companySymbolSearched.innerHTML = newText;
-                    }
+            let companyNameSearched = document.createElement("span");
+            companyNameSearched.setAttribute("id", "companyName");
+            companyNameSearched.innerHTML = data[i]["name"];
 
-                    searchedCompany.innerHTML = `<img id="imgId" onerror="this.src='https://i.ibb.co/TKpvydb/default.png'" src="${data2.profile["image"]}">` + companyNameSearched.innerHTML + companySymbolSearched.innerHTML + stockPercentages.innerHTML;
+            let companySymbolSearched = document.createElement("span");
+            companySymbolSearched.setAttribute("id", "companySymbol");
+            companySymbolSearched.innerHTML = "  (" + data[i]["symbol"] + ")  ";
 
-                    suggestions.appendChild(searchedCompany); 
-                });
-          }
-          hideLoading();
+            if (userInput !== "") {
+              let text = companyNameSearched.innerHTML;
+              let re = new RegExp(userInput, "gi");
+              let newText = text.replace(re, `<span id="mark">$&</span>`);
+              companyNameSearched.innerHTML = newText;
+            }
+
+            if (userInput !== "") {
+              let text = companySymbolSearched.innerHTML;
+              let re = new RegExp(userInput, "gi");
+              let newText = text.replace(re, `<span id="mark">$&</span>`);
+              companySymbolSearched.innerHTML = newText;
+            }
+
+            searchedCompany.innerHTML =
+              `<img id="imgId" onerror="this.src='https://i.ibb.co/TKpvydb/default.png'" src="${data2.profile["image"]}">` +
+              companyNameSearched.innerHTML +
+              companySymbolSearched.innerHTML +
+              stockPercentages.innerHTML;
+
+            suggestions.appendChild(searchedCompany);
+          });
+      }
+      loader.style.display = "none";
+      searchButton.style.display = "block";
     });
-    };
+};
 
-    searchButton.addEventListener("click", searchCompanies);
-    
+const urlMarquee =
+  "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/stock/list";
 
-    const urlMarquee = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/stock/list";
+let symbolsArray = [];
+let pricesArray = [];
 
-    let symbolsArray = [];
-    let pricesArray = [];
+fetch(urlMarquee)
+  .then((response3) => response3.json())
+  .then((data3) => {
+    for (let i = 0; i < 1000; i++) {
+      symbolsArray.push(data3[i]["symbol"]);
+      pricesArray.push(data3[i]["price"]);
 
-    fetch(urlMarquee)
-    .then ((response3) => response3.json())
-    .then ((data3) => {
+      const marqueeElement = document.createElement("span");
 
-        for (let i = 0; i < 1000; i++) {
-            symbolsArray.push(data3[i]["symbol"]);
-            pricesArray.push(data3[i]["price"]);
+      marqueeElement.innerHTML =
+        `<span><span id="marqueeCompanyName" class="marqueeElement first">` +
+        data3[i]["symbol"] +
+        `</span>` +
+        `<span id="marqueeCompanyPrice" class="marqueeElement second" style="color: #33D759FF"> $` +
+        data3[i]["price"] +
+        `</span></span>`;
 
-            const marqueeElement = document.createElement("span");
-
-            marqueeElement.innerHTML = `<span><span id="marqueeCompanyName" class="marqueeElement first">` + data3[i]["symbol"] + `</span>` + `<span id="marqueeCompanyPrice" class="marqueeElement second" style="color: #33D759FF"> $` + data3[i]["price"] + `</span></span>`;
-
-            marquee.appendChild(marqueeElement);
-        }
-    });
-
-
-    
-
-    
+      marquee.appendChild(marqueeElement);
+    }
+  });
